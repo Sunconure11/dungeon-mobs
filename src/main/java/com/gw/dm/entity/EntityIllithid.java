1bfd92aef480671304ce028ab16f0c9baaa8ff07
Sunconure11 <eyespot7b@aol.com>
2018-09-08 16:52:47 -0400

    actually fixed armor

--- src/main/java/com/gw/dm/entity/EntityIllithid.java
+++ src/main/java/com/gw/dm/entity/EntityIllithid.java
@@ -230,7 +230,7 @@ public class EntityIllithid extends EntityDungeonMob {
 					for (ItemStack slot : armors) {
 						if ((slot != null) && (slot.getItem() instanceof ItemArmor)) {
 							ItemArmor armor = (ItemArmor) slot.getItem();
-							if (world.isRemote && armor.getEquipmentSlot() == EntityEquipmentSlot.HEAD) {
+							if (armor.armorType == EntityEquipmentSlot.HEAD) {
 								hat = slot;
 								break;
 							}
