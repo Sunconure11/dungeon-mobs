18ce2db06b23a8a096ec53b3664d033ea8480470
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2019-08-28 06:37:10 -0400

    Version 5.7.3

--- src/main/java/com/gw/dm/DungeonMobs.java
+++ src/main/java/com/gw/dm/DungeonMobs.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 		dependencies = "after:thaumcraft; after:dldungeonsjbg")
 public class DungeonMobs {
 	public static final String MODID = "dungeonmobs";
-	public static final String VERSION = "5.7.2";
+	public static final String VERSION = "5.7.3";
 
 	@Instance(MODID)
 	public static DungeonMobs instance;
